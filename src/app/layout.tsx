cabf1b14fae8af0ca8782856b4e30ab00397b645
NaijasonEzeiru <jayseehe1035@gmail.com>
2025-09-08 10:37:47 +0100

    ...................

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import Header from "@/components/header";
+import Header from "../components/header";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
