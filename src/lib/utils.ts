a42eea25ffadcb7ac452d98e4543af572084aba7
NaijasonEzeiru <jayseehe1035@gmail.com>
2025-10-02 16:07:08 +0100

    ...................................

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// export const BASE_URL = "https://crypto-fund-nine.vercel.app";
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3001";
+export const BASE_URL = "https://crypto-fund-nine.vercel.app";
+// export const BASE_URL =
+//   process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3001";
